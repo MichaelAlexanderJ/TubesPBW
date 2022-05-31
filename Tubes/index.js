754de2792090248bbf63f3930ebdb34c8bb089c9
Ranks007 <Michaelalex2705@gmail.com>
2022-05-31 23:35:09 +0700

    latest

---
+++ Tubes/index.js
@@ -0,0 +1,44 @@
+import express, { query } from 'express';
+import path, { resolve } from 'path';
+import mysql from 'mysql';
+
+
+
+const PORT = 8080;
+const app = express();
+
+const publicPath = path.resolve('public');
+app.use(express.static(publicPath));
+
+app.set('view engine','ejs');
+app.use(express.urlencoded({ extended: true }));
+
+
+// ambil koneksi
+
+app.get('/home', async(req,res) => {
+    res.render('home', {
+            
+    });
+});
+
+app.get('/', async(req,res) => {
+    res.render('login', {
+            
+    });
+});
+
+
+app.get('/unggahTopik', async(req,res) => {
+    res.render('unggahTopik',{
+
+    });
+});
+
+app.post('/', async(req,res) => {
+    res.redirect('home');
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
