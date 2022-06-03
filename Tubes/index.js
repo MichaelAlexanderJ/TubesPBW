d2d72a7b156593374f597708f6ee65a4e200d394
Ranks007 <Michaelalex2705@gmail.com>
2022-06-03 13:40:35 +0700

    Update index.js

--- Tubes/index.js
+++ Tubes/index.js
@@ -1,8 +1,9 @@
 import express, { query } from 'express';
 import path, { resolve } from 'path';
 import mysql from 'mysql';
+import {router} from './router.js'
 
-
+app.use(router);
 
 const PORT = 8080;
 const app = express();
@@ -12,87 +13,6 @@ app.use(express.static(publicPath));
 
 app.set('view engine','ejs');
 app.use(express.urlencoded({ extended: true }));
-
-
-// Connect Database
-
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'DATATUBES'
-});
-
-const dbConnect = () => {
-    return new Promise((resolve,reject) => {
-        pool.getConnection((err,conn) => {
-            if(err){
-                reject(err);
-            }
-            else{
-                resolve(conn);
-                
-            }
-        })
-    })
-}
-
-
-// ambil koneksi
-
-app.get('/home', async(req,res) => {
-    const conn = await dbConnect();
-    conn.release();
-    res.render('home', {
-            
-    });
-});
-app.get('/homeAdmin', async(req,res) => {
-    const conn = await dbConnect();
-    conn.release();
-    res.render('homeAdmin', {
-            
-    });
-});
-
-app.get('/', async(req,res) => {
-    const conn = await dbConnect();
-    conn.release();
-    res.render('login', {
-            
-    });
-});
-
-
-app.get('/unggahTopik', async(req,res) => {
-    const conn = await dbConnect();
-    conn.release();
-    res.render('unggahTopik',{
-
-    });
-});
-
-app.get('/skripsiSaya', async(req,res) => {
-    const conn = await dbConnect();
-    conn.release();
-    res.render('topikSkripsiSaya',{
-
-    });
-});
-
-app.get('/kelolaAKun', async(req,res) => {
-    const conn = await dbConnect();
-    conn.release();
-    res.render('kelolaAkun',{
-
-    });
-});
-
-app.post('/', async(req,res) => {
-    const conn = await dbConnect();
-    conn.release();
-    res.redirect('homeAdmin');
-})
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
