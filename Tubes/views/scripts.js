952c1115b8f903f6a43143c153aa728efbcda61b
Ranks007 <Michaelalex2705@gmail.com>
2022-06-12 13:26:52 +0700

    test show div comments

---
+++ Tubes/views/scripts.js
@@ -0,0 +1,10 @@
+
+function myFunction() {
+  var x = document.getElementById("comments");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+console.log("jalan bangsat")
